463a303763b391e8e8bde94339582ae575d7ac74
Tarequl Islam <tarequlislam542@yahoo.com>
2021-09-18 13:06:54 +0600

    first flutter project

---
+++ new_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      home: Scaffold(
+        appBar: AppBar( title: Text("Home Page"),
+        ),
+        body: Text("Hello!This is the body of the App"),
+     // backgroundColor: Colors.black12,
+      ),
+     theme: ThemeData(scaffoldBackgroundColor: Colors.brown, primarySwatch: Colors.green ),
+
+
+
+    );
+
+  }
+}
+
